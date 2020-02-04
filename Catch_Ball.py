707e4349e58ff4d3fc53bebf573c84ecc8cb7e05
Serdyuk-prog <60281710+Serdyuk-prog@users.noreply.github.com>
2020-02-04 20:32:37 +0700

    Объектный рефакторинг

--- Catch_Ball.py
+++ Catch_Ball.py
@@ -1,39 +1,38 @@
 from tkinter import *
 import math
 from random import randrange as rnd, choice
-import time
+
 root = Tk()
-root.geometry('800x600')
 WIGHT = 800
 HIGHT = 600
-canv = Canvas(root,bg='white')
-canv.pack(fill=BOTH,expand=1)
+colors = ['red', 'orange', 'yellow', 'green', 'blue']
 
-colors = ['red','orange','yellow','green','blue']
+class Ball():
+    def __init__(self):
+        self.x = rnd(100, 700)
+        self.y = rnd(100, 500)
+        self.r = rnd(30, 50)
+        self.dx = +10
+        self.dy = +10
+        self.ball_id = canv.create_oval(self.x - self.r, self.y - self.r, self.x + self.r, self.y + self.r,
+                                        fill=choice(colors), width=0)
+    def move(self):
+        self.x += self.dx
+        self.y += self.dy
+        if self.x + self.r >= WIGHT or self.x - self.r <= 0:
+            self.dx = -self.dx
+        if self.y + self.r > HIGHT or self.y - self.r <= 0:
+            self.dy = -self.dy
+
+    def show(self):
+        canv.move(self.ball_id, self.dx, self.dy)
+
 
 def tick():
-    global x, y, r, dx, dy
-    x += dx
-    y += dy
-    if x + r >= WIGHT or x - r <= 0:
-        dx = -dx
-    if y + r > HIGHT or y - r <= 0:
-        dy = -dy
-    canv.move(ball_id, dx, dy)
+    ball.move()
+    ball.show()
     root.after(50, tick)
 
-
-def new_ball():
-    global x, y, r, ball_id, dx, dy
-    canv.delete(ALL)
-    x = rnd(100,700)
-    y = rnd(100,500)
-    r = rnd(30,50)
-    dx = +10
-    dy = +10
-    ball_id = canv.create_oval(x-r,y-r,x+r,y+r,fill = choice(colors), width=0)
-
-    #root.after(1000,new_ball)
 
 def click(event):
     x_mous, y_mous = event.x, event.y
@@ -41,11 +40,15 @@ def click(event):
         print('OK')
 
 def main():
+    global canv, ball
+
+    root.geometry(str(WIGHT) + 'x' + str(HIGHT))
+    canv = Canvas(root, bg='white')
+    canv.pack(fill=BOTH, expand=1)
     canv.bind('<Button-1>', click)
-    new_ball()
+    ball = Ball()   
     tick()
+    mainloop()
 
 
 main()
-
-mainloop()
